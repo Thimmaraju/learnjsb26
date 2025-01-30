af6ac24c425442f676d51b7843f3cf397e6fe4a1
G Thimmaraju <thimmaraju418@gmail.com>
2025-01-30 14:43:25 +0530

    update

--- callBacks.js
+++ callBacks.js
@@ -140,6 +140,37 @@
 // const greet = outer(); // outer() runs, but doesn't log yet
 // greet()
 
+// 1. Passing 10 as the initial value to Step1
+// 2. If there is no error, then passing the result to Step2
+// 3. If there is no error again, then passing the result to Step3
+// 4. Finally, printing the result.
+
+step1(10, function(result1, error) {
+  if(!error) {
+      step2(result1, function (result2, error) {
+          if(!error) {
+              step3(result2, function (result3, error) {
+                  if(!error) {
+                      console.log("Results "+result3);
+                  }
+              })
+          }
+      })
+  }
+});
+
+function step1 (value, callback) {
+  callback(value + 10, false);
+}
+
+function step2 (value, callback) {
+  callback(value + 10, false);
+}
+
+function step3 (value, callback) {
+  callback(value + 10, false);
+}
+
 
 //Using multiple nested callbacks can become complex and hard to manage, often referred to as "callback hell."
 
--- promises.js
+++ promises.js
@@ -33,3 +33,42 @@ const task = new Promise((resolve, reject) => {
   //or
 
   //task.then(message => console.log(message), errormsg => console.log(errormsg))
+
+   //==================================================
+
+   function step1(value, error) {
+    return new Promise((resolve, reject) => {
+      if (error) { 
+        reject('Something went wrong');
+      } else {
+        resolve(value + 10); 
+      }
+    });
+  }
+  
+  function step2(value, error) {
+    return new Promise((resolve, reject) => {
+      if (error) { 
+        reject('Something went wrong');
+      } else {
+        resolve(value + 10); 
+      }
+    });
+  }
+  
+  function step3(value, error) {
+    return new Promise((resolve, reject) => {
+      if (error) { 
+        reject('Something went wrong');
+      } else {
+        resolve(value + 10);
+      }
+    });
+  }
+  
+  // Usage:
+  step1(10, false)
+    .then((result1) => step2(result1, false))
+    .then((result2) => step3(result2, false))
+    .then((result3) => console.log(result3))
+    .catch((error) => console.log(error));
